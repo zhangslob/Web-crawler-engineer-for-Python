aef03890bfb17caf6b919b33f5f6b2be23c6e04c
Draven <zhangslob@gmail.com>
2017-10-27 19:05:18 +0800

    Update spider.py

--- 第二课_初识Python爬虫/spider.py
+++ 第二课_初识Python爬虫/spider.py
@@ -6,12 +6,13 @@ import requests
 r = requests.get('https://book.douban.com/subject/1084336/comments/').text
 
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(r,'lxml')
-pattern = soup.find_all('p','comment-content')
+soup = BeautifulSoup(r, 'lxml')
+pattern = soup.find_all('p', 'comment-content')
 for item in pattern:
     print(item.string)
 
 import pandas
+
 comments = []
 for item in pattern:
     comments.append(item.string)
